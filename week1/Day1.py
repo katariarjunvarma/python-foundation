ce829980b28f3ff8b332676fb2d61c9b0cc351a0
Arjun Varma <arjunvarma5110@gmail.com>
2026-02-03 15:42:47 +0530

    Day 1 Python basics

---
+++ week1/Day1.py
@@ -0,0 +1,11 @@
+name = "Arjun"
+Roll_number = 29
+percentage = 80.0
+
+print(Roll_number + 10)
+print(type(name))
+print(type(percentage))
+print(type(Roll_number))
+print(percentage / 2)
+print(name + name)
+print(name + " " + str(Roll_number)) 
