cdd6927b2a96c8f57e20eea377f824342e7330a1
Arjun Varma <arjunvarma5110@gmail.com>
2026-02-04 14:41:58 +0530

    Done another problem

--- week1/Day2.py
+++ week1/Day2.py
@@ -1,6 +1,16 @@
+# PRACTICE PROBLEM 1 — Basic Math & Input
+
 x = int(input("Enter a number: " ))
 y = int(input("Enter another number: "))
 print("Addition of these numbers", x + y)
 print("Subtraction of theses numbers", x - y)
 print("Multiplication of these numbers", x * y)
 print("Divison of these numbers", x / y)
+print("Average of these numbers", (x + y) / 2)
+
+# PRACTICE PROBLEM 2 — String + Number Handling
+
+a = input("Enter your Name: ")
+b = int(input("Enter your birth year: "))
+print("Your current age is", 2025 - b)
+print(a * 2)
