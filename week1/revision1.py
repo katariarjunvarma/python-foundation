1365e21d7b84c8f5857bbf0d042a6fac8780948d
Arjun Varma <arjunvarma5110@gmail.com>
2026-02-10 16:52:21 +0530

    Done Revision of Day 1-4

---
+++ week1/revision1.py
@@ -0,0 +1,114 @@
+# n = int(input("Enter a number: "))
+# if n % 2 == 0:
+#     print("Even")
+# else:
+#     print("Odd")
+
+
+# n = int(input("Enter a number: "))
+# for i in range(1, n+1):
+#     print(i)
+
+
+# n = int(input("Enter a number: "))
+# total = 0
+# for i in range(1, n + 1):
+#     total = total + i
+# print(total)
+
+
+# for i in range(1,4):
+#     for j in range(1,6):
+#         print("*", end=" ")
+#     print()
+
+
+# for i in range(1,5):
+#     for j in range(1, i + 1):
+#         print(j, end = " ")
+#     print()
+
+
+# for i in range(4, 0, -1):
+#     for j in range(1, i + 1):
+#         print("*" ,end = "")
+#     print()
+
+
+# n = int(input("Enter a number: "))
+# if n % 2 == 0:
+#     print("Even")
+# else:
+#     print("Odd")
+
+
+# for i in range(1, 11):
+#     print(i)
+
+
+# total = 0
+# for i in range(1, 11):
+#      total = total + i 
+# print(total)
+
+
+# for i in range(1, 5):
+#     for j in range(1,i + 1):
+#         print("*", end="")
+#     print()
+
+
+# for i in range(4, 0, -1):
+#     for j in range(1, i +1):
+#         print("*", end="")
+#     print()
+
+
+# for i in range(1,5):
+#     for j in range(1, i +1):
+#         print(j, end="")
+#     print()
+
+
+# n = int(input("Enter a number: "))
+# print( n ** 2)
+# print( n ** 3)
+# print(type(n))
+
+
+# n= int(input("Enter a number: "))
+# if n > 0:
+#     print("Positive")
+# elif n < 0:
+#     print("Negative")
+# else:
+#     print("Zero")
+
+
+# n= int(input("Enter a number: "))
+# for i in range(1, n + 1):
+#     print(i)
+
+
+# n= int(input("Enter a number: "))
+# total = 0
+# for i in range(1, n + 1):
+#     total = total + i
+# print(total)
+
+
+# n= int(input("Enter a number: "))
+# for i in range(1, n+1):
+#     if i % 2 != 0:
+#         print(i)
+
+# for i in range(1,6):
+#     for j in range (1, i +1):
+#         print("*", end="")
+#     print()
+
+
+# n = int(input("Enter a number: "))
+# for i in range(n, 0, -1):
+#     if i % 2 ==0:
+#         print(i)
